2eeb577a3054ab01466270ededb0b3d133469c06
g0sha1337 <90519734+g0sha1337@users.noreply.github.com>
2025-03-23 01:34:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-#Config file with your secret telegram token
-<<<<<<< HEAD
 TelegramBotToken = 'your_token' #from @BotFather
 disable_web_page_preview=True # links in message without links, true for default
-=======
-TelegramBotToken = 'Your_secret_tgbot_token' #from @BotFather
->>>>>>> parent of f94c4f9 (add disable_web_page_preview parameter)
